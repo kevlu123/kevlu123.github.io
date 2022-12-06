fa63bf5705b37c016d88bece889445af6b3f11ae
Kevin Lu <kevlu321@gmail.com>
2022-12-06 18:56:20 +1300

    Increase wasm memory

--- wings/repl/build.sh
+++ wings/repl/build.sh
@@ -1,3 +1,4 @@
 emcc main.cpp -o wings.js -std=c++20 -O3 -s WASM=1 \
+    -s TOTAL_MEMORY=64MB \
     -s EXPORTED_RUNTIME_METHODS='["allocateUTF8","UTF8ToString"]' \
     -s EXPORTED_FUNCTIONS='["_free", "_OpenRepl", "_CloseRepl", "_GetStdout", "_Execute", "_ShouldExit"]'
